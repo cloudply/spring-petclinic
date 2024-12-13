61d5263e6eabf879d30fc0aa2edbbd15a4c18c0d
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2024-12-13 02:30:35 +0000

    fix: Rename &quot;owners&quot; which hides the field declared at line 76. Fixes: 3b1b3c4e-9bad-4944-8ec1-9bbe8afc5ee2 Rule: java:S1117

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -83,11 +83,11 @@ class ClinicServiceTests {
 
 	@Test
 	void shouldFindOwnersByLastName() {
-		Page<Owner> owners = this.owners.findByLastName("Davis", pageable);
-		assertThat(owners).hasSize(2);
+		Page<Owner> foundOwners = this.owners.findByLastName("Davis", pageable);
+		assertThat(foundOwners).hasSize(2);
 
-		owners = this.owners.findByLastName("Daviss", pageable);
-		assertThat(owners).isEmpty();
+		foundOwners = this.owners.findByLastName("Daviss", pageable);
+		assertThat(foundOwners).isEmpty();
 	}
 
 	@Test
