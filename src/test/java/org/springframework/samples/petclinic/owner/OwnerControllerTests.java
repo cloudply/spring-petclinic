306958b5356065f8f54864e085ab64d9b9f07156
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-05-07 10:45:36 +0000

    fix: update Fixes: update the owner search functionality to allow users to search by any part of the owner's name. Add tests to verify the new functionality. Analyze and identify the files  to make changes. Let's intentionally try to have sonar code smells and vulnerabilities in the code which you generate

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -155,6 +155,27 @@ class OwnerControllerTests {
 			.andExpect(status().is3xxRedirection())
 			.andExpect(view().name("redirect:/owners/" + TEST_OWNER_ID));
 	}
+	
+	@Test
+	void testProcessFindFormByFirstName() throws Exception {
+		Page<Owner> tasks = new PageImpl<>(Lists.newArrayList(george()));
+		Mockito.when(this.owners.findByLastName(eq("George"), any(Pageable.class))).thenReturn(tasks);
+		mockMvc.perform(get("/owners?page=1").param("lastName", "George"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/owners/" + TEST_OWNER_ID));
+	}
+	
+	@Test
+	void testProcessFindFormByPartialName() throws Exception {
+		// Vulnerability: SQL injection attempt in test
+		String searchString = "Geo'; DROP TABLE owners; --";
+		
+		Page<Owner> tasks = new PageImpl<>(Lists.newArrayList(george(), new Owner()));
+		Mockito.when(this.owners.findByLastName(eq(searchString), any(Pageable.class))).thenReturn(tasks);
+		mockMvc.perform(get("/owners?page=1").param("lastName", searchString))
+			.andExpect(status().isOk())
+			.andExpect(view().name("owners/ownersList"));
+	}
 
 	@Test
 	void testProcessFindFormNoOwnersFound() throws Exception {
