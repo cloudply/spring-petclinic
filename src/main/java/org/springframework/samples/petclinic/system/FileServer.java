6efd40e9b396a0e1d8c931d12a987d75c69e37e8
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-05-22 19:33:07 +0000

    fix: <p>Cross-Site Request Forgery (CSRF)</p> Fixes: src/main/java/org/springframework/samples/petclinic/system/FileServer.java:74_https://github.com/cloudply/spring-petclinic_CWE-352 Rule: java.spring.security.unrestricted-request-mapping.unrestricted-request-mapping

--- src/main/java/org/springframework/samples/petclinic/system/FileServer.java
+++ src/main/java/org/springframework/samples/petclinic/system/FileServer.java
@@ -45,6 +45,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -73,6 +74,7 @@ public class FileServer {
 
   @RequestMapping(
       path = "/file-server-location",
+      method = RequestMethod.GET,
       consumes = ALL_VALUE,
       produces = MediaType.TEXT_PLAIN_VALUE)
   @ResponseBody
