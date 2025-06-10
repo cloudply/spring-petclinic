8662e5ec57c62c601ee702a71e109e949e351207
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-06-10 17:21:43 +0000

    fix: Use already-defined constant 'TELEPHONE' instead of duplicating its value here. Fixes: 7eba87a5-200b-4776-8161-f8f6b51449fb Rule: java:S1192

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -223,7 +223,7 @@ class OwnerControllerTests {
 			.andExpect(model().attribute("owner", hasProperty("firstName", is("George"))))
 			.andExpect(model().attribute("owner", hasProperty("address", is("110 W. Liberty St."))))
 			.andExpect(model().attribute("owner", hasProperty("city", is("Madison"))))
-			.andExpect(model().attribute("owner", hasProperty("telephone", is("6085551023"))))
+			.andExpect(model().attribute("owner", hasProperty(TELEPHONE, is("6085551023"))))
 			.andExpect(model().attribute("owner", hasProperty("pets", not(empty()))))
 			.andExpect(model().attribute("owner",
 					hasProperty("pets", hasItem(hasProperty("visits", hasSize(greaterThan(0)))))))
