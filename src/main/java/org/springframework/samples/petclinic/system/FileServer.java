23f5a8243cdb77824a7158476b8a39bda8e88174
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-06-25 16:45:02 +0000

    fix: <p>Relative Path Traversal</p> Fixes: src/main/java/org/springframework/samples/petclinic/system/FileServer.java:97_https://github.com/cloudply/spring-petclinic_CWE-23 Rule: java.spring.security.injection.tainted-file-path.tainted-file-path

--- src/main/java/org/springframework/samples/petclinic/system/FileServer.java
+++ src/main/java/org/springframework/samples/petclinic/system/FileServer.java
@@ -90,7 +90,8 @@ public class FileServer {
     // DO NOT use multipartFile.transferTo(), see
     // https://stackoverflow.com/questions/60336929/java-nio-file-nosuchfileexception-when-file-transferto-is-called
     try (InputStream is = multipartFile.getInputStream()) {
-      var destinationFile = destinationDir.toPath().resolve(multipartFile.getOriginalFilename());
+      String filename = new File(multipartFile.getOriginalFilename()).getName();
+      var destinationFile = destinationDir.toPath().resolve(filename);
       Files.deleteIfExists(destinationFile);
       Files.copy(is, destinationFile);
     }
