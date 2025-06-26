06d668161aae61f83804902902a7b166bee40f43
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-06-26 18:44:08 +0000

    fix: Define a constant instead of duplicating this literal &quot;/owners/{ownerId}/pets/{petId}/edit&quot; 3 times. Fixes: 52e6a4ed-eb88-4bf7-8354-318d6f689578 Rule: java:S1192

--- src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
@@ -56,6 +56,7 @@ class PetControllerTests {
 	private static final String BIRTH_DATE = "birthDate";
 	
 	private static final String PETS_NEW_FORM_URL = "/owners/{ownerId}/pets/new";
+	private static final String PETS_EDIT_FORM_URL = "/owners/{ownerId}/pets/{petId}/edit";
 	private static final String PETS_FORM_VIEW = "pets/createOrUpdatePetForm";
 
 	@Autowired
@@ -110,7 +111,7 @@ class PetControllerTests {
 
 	@Test
 	void testInitUpdateForm() throws Exception {
-		mockMvc.perform(get("/owners/{ownerId}/pets/{petId}/edit", TEST_OWNER_ID, TEST_PET_ID))
+		mockMvc.perform(get(PETS_EDIT_FORM_URL, TEST_OWNER_ID, TEST_PET_ID))
 			.andExpect(status().isOk())
 			.andExpect(model().attributeExists("pet"))
 			.andExpect(view().name(PETS_FORM_VIEW));
@@ -119,7 +120,7 @@ class PetControllerTests {
 	@Test
 	void testProcessUpdateFormSuccess() throws Exception {
 		mockMvc
-			.perform(post("/owners/{ownerId}/pets/{petId}/edit", TEST_OWNER_ID, TEST_PET_ID).param("name", PET_NAME)
+			.perform(post(PETS_EDIT_FORM_URL, TEST_OWNER_ID, TEST_PET_ID).param("name", PET_NAME)
 				.param("type", PET_TYPE_NAME)
 				.param(BIRTH_DATE, PET_BIRTH_DATE))
 			.andExpect(status().is3xxRedirection())
@@ -129,7 +130,7 @@ class PetControllerTests {
 	@Test
 	void testProcessUpdateFormHasErrors() throws Exception {
 		mockMvc
-			.perform(post("/owners/{ownerId}/pets/{petId}/edit", TEST_OWNER_ID, TEST_PET_ID).param("name", PET_NAME)
+			.perform(post(PETS_EDIT_FORM_URL, TEST_OWNER_ID, TEST_PET_ID).param("name", PET_NAME)
 				.param(BIRTH_DATE, "2015/02/12"))
 			.andExpect(model().attributeHasNoErrors("owner"))
 			.andExpect(model().attributeHasErrors("pet"))
