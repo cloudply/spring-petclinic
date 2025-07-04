6c4ef14d779957b943c4f5f8bfd626bf5f579a17
vishalkumar437 <vishalkumar437@gmail.com>
2025-07-04 21:31:16 +0530

    Revert "Revert "this is sast code smell code""
    This reverts commit 1ec621eddeaa89df0032aa86e0a9c6b21ad683ec.

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,41 +1,43 @@
-/*
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.Locale;
-
-/**
- * PetClinic Spring Boot Application.
- *
- * @author Dave Syer
- *
- */
 @SpringBootApplication
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
+		
+		// Code smells for SAST
+		int unused = 42; // unused variable
+
+		String dbPassword = "admin123"; // hardcoded password
+
+		try {
+			throw new RuntimeException("Trigger");
+		} catch (Exception e) {
+			// empty catch block
+		}
 	}
 
+	public void x() {
+		System.out.println("Poorly named method");
+	}
+
+	public void complexLogic() {
+		for (int i = 0; i < 5; i++) {
+			if (i % 2 == 0) {
+				System.out.println("Even");
+			} else {
+				if (i == 3) {
+					System.out.println("Three");
+				} else {
+					System.out.println("Odd");
+				}
+			}
+		}
+	}
 }
